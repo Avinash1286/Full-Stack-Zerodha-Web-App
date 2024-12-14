2b5e140fe82a4e7b6ffd277b89afb47bc7252c23
Avinash1286 <abinashyadav3.141@gmail.com>
2024-12-14 21:00:22 +0545

    first commit

---
+++ frontend/src/landing_page/home/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Awards from './Awards'
+import Education from './Education'
+import Hero from './Hero'
+import Pricing from './Pricing'
+import Stats from './Stats'
+import NavBar from '../Navbar'
+import Footer from '../Footer'
+import OpenAccount from '../OpenAccount'
+
+function HomePage() {
+  return (
+    <>
+    
+    <Hero/>
+    <Awards/>
+    <Stats/>
+    <Pricing/>
+    <Education/>
+    <OpenAccount/>
+    
+    </>
+  )
+}
+
+export default HomePage
